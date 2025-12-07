1529b81ac5a9b58434c946d88e772f87e6a28b84
Gideon <gideonj2707@gmail.com>
2025-12-07 20:17:45 +0200

    Enhance index.html layout and styling; add main.js data type comments

---
+++ main.js
@@ -0,0 +1,11 @@
+//variables
+
+//data types
+//string "Hello World" `format string`
+//number 1234 12.34
+//boolean true false
+
+//object {name: "John", age: 30}
+//array [1, 2, 3, 4, 5]
+//null
+
